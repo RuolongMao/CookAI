8ddf77e53fa818a0c1dd119f8a6ad46bd420356d
Dave <wangebb@bc.edu>
2024-10-10 17:21:01 -0400

    NavBar CSS

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import "../css/NavBar.css"
+import fork from "../images/fork.jpg"
 
 
 // 设置NavBar组件，接受方法和参数）
@@ -16,20 +18,56 @@ const Navbar = ({ isLoggedIn, username, onLogout }) => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">CookingAI</a>
-      <div className="ml-auto">
+      <div className="container">
+        {/* 第一列: 左边 */}
+        <div className="col">
+
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        {/* 中间列: 两行 */}
+        <div className="col text-center">
+          <div className="row fork-pic">
+            <a href="#">
+            <img src={fork} alt="fork-pic"/>
+            </a>
+            
+          </div>
+          <div className="row">
+           <a className="name" href="#">
+            CHEFBOTX
+           </a>
+          </div>
+        </div>
+
+        {/* 第三列: 右边 */}
+        <div className="col text-right">
+
         {isLoggedIn ? (
-          <>
-          <span className="navbar-text">Hello, {username}!</span>
-          <button className="btn btn-link" onClick={handleLogout}>Logout</button>
-          </>
-        ) : (
+              <>
+                <span className="navbar-text">Hello, {username}!</span>
+              </>
+            ) : (
+              <>
+                <span className="navbar-text">Hello, Guest!</span>
+              </>
+            )}
+
+
+
+          {isLoggedIn ? (
             <>
-            <span className="navbar-text">Hello, Guest!</span>
-            <Link to="/register" className="btn btn-link">Register</Link>
-            <Link to="/signin" className="btn btn-link">Sign In</Link>
+              <button className="btn btn-link" onClick={handleLogout}>Logout</button>
             </>
-        )}
+          ) : (
+            <>
+              <Link to="/register" className="btn btn-link">Register</Link>
+              <Link to="/signin" className="btn btn-link">Sign In</Link>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
